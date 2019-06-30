72560106cabb810f0039898d89280405911a33da
jy1dyz <jyldyz.kgz@gmail.com>
2019-07-01 02:33:12 +0600

    fix the function toInitials

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -1,18 +1,21 @@
 package ru.skillbranch.devintensive.utils
 
+import android.text.TextUtils.isEmpty
+
 object Utils {
-    fun parseFullName(fullName:String?):Pair<String?, String?> {
+    fun parseFullName(fullName: String?): Pair<String?, String?> {
         val parts: List<String>? = fullName?.split(" ")
         val firstName = parts?.getOrNull(0)
         val lastName = parts?.getOrNull(1)
 
 
-        if("$fullName".isBlank()) return  Pair(null, null)
+        if ("$fullName".isBlank()) return Pair(null, null)
         return Pair(firstName, lastName)
         //return firstName to lastName
 
     }
-    fun transliteration(payload: String, divider:String = " "): String {
+
+    fun transliteration(payload: String, divider: String = " "): String {
         /*val translit: HashMap<String, String> = hashMapOf(
             "а" to "a",
             "б" to "b",
@@ -63,72 +66,72 @@ object Utils {
         newTyping.forEach{new.append(it)}
         return new.toString() */
         return payload.replace(" ", divider)
-            .replace("А", "A")
-            .replace("Б", "B")
-            .replace("В", "V")
-            .replace("Г", "G")
-            .replace("Д", "D")
-            .replace("Е", "E")
-            .replace("Ё", "E")
-            .replace("Ж", "ZH")
-            .replace("З", "Z")
-            .replace("И", "I")
-            .replace("Й", "I")
-            .replace("К", "K")
-            .replace("Л", "L")
-            .replace("М", "M")
-            .replace("Н", "N")
-            .replace("О", "O")
-            .replace("П", "P")
-            .replace("Р", "R")
-            .replace("С", "S")
-            .replace("Т", "T")
-            .replace("У", "U")
-            .replace("Ф", "F")
-            .replace("Х", "H")
-            .replace("Ц", "C")
-            .replace("Ч", "CH")
-            .replace("Ш", "SH")
-            .replace("Щ", "SH")
-            .replace("Ъ", "")
-            .replace("Ы", "I")
-            .replace("Ь", "")
-            .replace("Э", "E")
-            .replace("Ю", "YU")
-            .replace("Я", "YA")
-            .replace("а", "a")
-            .replace("б", "b")
-            .replace("в", "v")
-            .replace("г", "g")
-            .replace("д", "d")
-            .replace("е", "e")
-            .replace("ё", "e")
-            .replace("ж", "zh")
-            .replace("з", "z")
-            .replace("и", "i")
-            .replace("й", "i")
-            .replace("к", "k")
-            .replace("л", "l")
-            .replace("м", "m")
-            .replace("н", "n")
-            .replace("о", "o")
-            .replace("п", "p")
-            .replace("р", "r")
-            .replace("с", "s")
-            .replace("т", "t")
-            .replace("у", "u")
-            .replace("ф", "f")
-            .replace("х", "h")
-            .replace("ц", "c")
-            .replace("ч", "ch")
-            .replace("ш", "sh")
-            .replace("щ", "sh")
-            .replace("ъ", "")
-            .replace("ы", "i")
-            .replace("ь", "")
-            .replace("э", "e")
-            .replace("ю", "yu")
-            .replace("я", "ya")
+                .replace("А", "A")
+                .replace("Б", "B")
+                .replace("В", "V")
+                .replace("Г", "G")
+                .replace("Д", "D")
+                .replace("Е", "E")
+                .replace("Ё", "E")
+                .replace("Ж", "ZH")
+                .replace("З", "Z")
+                .replace("И", "I")
+                .replace("Й", "I")
+                .replace("К", "K")
+                .replace("Л", "L")
+                .replace("М", "M")
+                .replace("Н", "N")
+                .replace("О", "O")
+                .replace("П", "P")
+                .replace("Р", "R")
+                .replace("С", "S")
+                .replace("Т", "T")
+                .replace("У", "U")
+                .replace("Ф", "F")
+                .replace("Х", "H")
+                .replace("Ц", "C")
+                .replace("Ч", "CH")
+                .replace("Ш", "SH")
+                .replace("Щ", "SH")
+                .replace("Ъ", "")
+                .replace("Ы", "I")
+                .replace("Ь", "")
+                .replace("Э", "E")
+                .replace("Ю", "YU")
+                .replace("Я", "YA")
+                .replace("а", "a")
+                .replace("б", "b")
+                .replace("в", "v")
+                .replace("г", "g")
+                .replace("д", "d")
+                .replace("е", "e")
+                .replace("ё", "e")
+                .replace("ж", "zh")
+                .replace("з", "z")
+                .replace("и", "i")
+                .replace("й", "i")
+                .replace("к", "k")
+                .replace("л", "l")
+                .replace("м", "m")
+                .replace("н", "n")
+                .replace("о", "o")
+                .replace("п", "p")
+                .replace("р", "r")
+                .replace("с", "s")
+                .replace("т", "t")
+                .replace("у", "u")
+                .replace("ф", "f")
+                .replace("х", "h")
+                .replace("ц", "c")
+                .replace("ч", "ch")
+                .replace("ш", "sh")
+                .replace("щ", "sh")
+                .replace("ъ", "")
+                .replace("ы", "i")
+                .replace("ь", "")
+                .replace("э", "e")
+                .replace("ю", "yu")
+                .replace("я", "ya")
 
 
     }
@@ -136,25 +139,28 @@ object Utils {
     fun toInitials(firstName: String?, lastName: String?): String? {
 
         val str = StringBuilder()
-        when(firstName){
-            null -> {}
-            "" -> {}
-            " " -> {}
-            "  " -> {}
-            else -> str.append("$firstName".substring(0,1).capitalize())
+
+        when {
+            firstName.isNullOrBlank() -> {
+            }
+            firstName.isNullOrEmpty() -> {
+            }
+            else -> str.append("$firstName".substring(0, 1).capitalize())
         }
-        when(lastName){
-            null -> {}
-            "" -> {}
-            " " -> {}
-            "  " -> {}
-            else -> str.append("$lastName".substring(0,1).capitalize())
+
+        when {
+            lastName.isNullOrBlank() -> {
+            }
+            lastName.isNullOrEmpty() -> {
+            }
+            else -> str.append("$lastName".substring(0, 1).capitalize())
         }
-        return if(str.isEmpty())
+        return if (str.isEmpty())
             "null"
         else
             str.toString()
 
     }
 }
+
 

412435c60cff8d998739cddd49a50b9070e9fb1d
jy1dyz <jyldyz.kgz@gmail.com>
2019-07-01 21:29:19 +0600

    realization of the pattern Builder

--- app/src/main/java/ru/skillbranch/devintensive/models/User.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/User.kt
@@ -57,6 +57,31 @@ data class User( //первичный конструктор
             isOnline: = $isOnline: 
         """.trimIndent()) */
 
+    class Builder{
+        private var id:String = ""
+        private var firstName:String? = ""
+        private var lastName: String? = ""
+        private var avatar: String? = ""
+        private var rating: Int = 0
+        private var respect: Int = 0
+        private var lastVisit: Date? = Date()
+        private var isOnline: Boolean = false
+
+        fun id(id:String) = apply{this.id = id}
+        fun firstName(firstName: String?) = apply{this.firstName = firstName}
+        fun lastName(lastName: String?) = apply{this.lastName = lastName}
+        fun avatar(avatar: String?) = apply{this.avatar = avatar}
+        fun rating(rating: Int) = apply{this.rating = rating}
+        fun respect(respect: Int) = apply{this.respect = respect}
+        fun lastVisit(lastVisit:Date) = apply{this.lastVisit = lastVisit}
+        fun isOnline(isOnline:Boolean) = apply{this.isOnline = isOnline}
+
+
+        fun build(): User{
+            return User(id,firstName,lastName,avatar,rating,respect,lastVisit,isOnline)
+        }
+
+    }
     companion object Factory {
         private var lastId:Int = -1
         fun makeUser(fullName:String?): User {
@@ -73,7 +98,8 @@ data class User( //первичный конструктор
             return User(id = "$lastId", firstName = firstName, lastName = lastName)
 
         }
-    }
 
+    }
 }
+
 

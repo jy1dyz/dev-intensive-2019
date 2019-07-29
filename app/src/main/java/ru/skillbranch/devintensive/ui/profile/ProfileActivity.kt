30bfd0648157da63192481824e8e616319f45076
jy1dyz <jyldyz.kgz@gmail.com>
2019-07-29 20:03:41 +0600

    added resources, sharedPreferences, edit profile, switch day/night theme

--- app/src/main/java/ru/skillbranch/devintensive/ui/profile/ProfileActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/ui/profile/ProfileActivity.kt
@@ -1,23 +1,35 @@
 package ru.skillbranch.devintensive.ui.profile
 
 
+import android.graphics.ColorFilter
+import android.graphics.PorterDuff
+import android.graphics.PorterDuffColorFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.util.Log
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import kotlinx.android.synthetic.main.activity_profile.*
 
 
 import ru.skillbranch.devintensive.R
+import ru.skillbranch.devintensive.models.Profile
+import ru.skillbranch.devintensive.viewmodels.ProfileViewModel
 
 
 @Suppress("NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
 class ProfileActivity : AppCompatActivity() {
-    /*companion object {
+    companion object {
         const val IS_EDIT_MODE = "IS_EDIT_MODE"
     }
 
 
+    private lateinit var viewModel: ProfileViewModel
     var isEditMode = false
-    lateinit var viewFields: Map<String, TextView> */
+    lateinit var viewFields: Map<String, TextView>
 
     /**
      * Вызывается при первом создании или перезапуске Activity
@@ -36,13 +48,16 @@ class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
-        //initViews(savedInstanceState)
+        initViews(savedInstanceState)
+        initViewModel()
+        Log.d("M_ProfileActivity", "onCreate")
 
     }
 
-    /* private fun initViews(savedInstanceState: Bundle?) {
+     private fun initViews(savedInstanceState: Bundle?) {
+
         viewFields = mapOf(
-            "nickname" to tv_nick_name,
+            "nickName" to tv_nick_name,
             "rank" to tv_rank,
             "firstName" to et_first_name,
             "lastName" to et_last_name,
@@ -52,17 +67,56 @@ class ProfileActivity : AppCompatActivity() {
             "respect" to tv_respect
         )
 
+         isEditMode = savedInstanceState?.getBoolean(IS_EDIT_MODE, false) ?: false
+         showCurrentMode(isEditMode)
 
 
-        btn_edit.setOnClickListener(View.OnClickListener {
-            isEditMode = !isEditMode
-            showCurrentMode(isEditMode)
-        })
-    } */
+         btn_edit.setOnClickListener {
+             if(isEditMode) saveProfileInfo()
+             isEditMode = !isEditMode
+             showCurrentMode(isEditMode)
+         }
 
-   /* private fun showCurrentMode(editMode: Boolean) {
+         btn_switch_theme.setOnClickListener {
+             viewModel.switchTheme()
+         }
+     }
 
-    } */
+    private fun showCurrentMode(isEdit: Boolean) {
+        val info = viewFields.filter {
+                setOf("firstName", "lastName", "about", "repository").contains(it.key) }
+        for((_,v)in info) {
+            v as EditText
+            v.isFocusable = isEdit
+            v.isFocusableInTouchMode = isEdit
+            v.isEnabled = isEdit
+            v.background.alpha = if(isEdit) 255 else 0
+        }
+
+        ic_eye.visibility = if(isEdit) View.GONE else View.VISIBLE
+        wr_about.isCounterEnabled = isEdit
+
+        with(btn_edit){
+            val filter: ColorFilter? = if(isEdit){
+                PorterDuffColorFilter(
+                    resources.getColor(R.color.color_accent, theme),
+                    PorterDuff.Mode.SRC_IN
+                )
+            } else {
+                null
+            }
+
+            val icon = if(isEdit){
+                resources.getDrawable(R.drawable.ic_save_black_24dp, theme)
+            }else {
+                resources.getDrawable(R.drawable.ic_edit_black_24dp, theme)
+            }
+
+            background.colorFilter = filter
+            setImageDrawable(icon)
+        }
+    }
+
 
 
     /**
@@ -75,7 +129,38 @@ class ProfileActivity : AppCompatActivity() {
 
     override fun onSaveInstanceState(outState: Bundle?) {
         super.onSaveInstanceState(outState)
+        outState?.putBoolean(IS_EDIT_MODE, isEditMode)
 
+    }
+
+    private fun initViewModel() {
+        viewModel  = ViewModelProviders.of(this).get(ProfileViewModel::class.java)
+        viewModel.getProfileData().observe(this, Observer { updateUI(it) })
+        viewModel.getTheme().observe(this, Observer { updateTheme(it) })
+    }
+
+    private fun updateTheme(mode: Int) {
+        Log.d("M_ProfileActivity", "updateTheme")
+        delegate.setLocalNightMode(mode)
+    }
+
+    private fun updateUI(profile: Profile) {
+        profile.toMap().also{
+            for ((k,v) in viewFields){
+                v.text = it[k].toString()
+            }
+        }
+    }
+
+    private fun saveProfileInfo() {
+        Profile(
+            firstName = et_first_name.text.toString(),
+            lastName = et_last_name.text.toString(),
+            about = et_about.text.toString(),
+            repository = et_repository.text.toString()
+        ).apply {
+            viewModel.saveProfileData(this)
+        }
     }
 
 }
